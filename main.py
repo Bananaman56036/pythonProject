69689dc2d350f4324d3eee2fadd135d536c0d126
Bananaman56036 <marcellus.whale@online.island.edu.hk>
2023-05-04 10:13:47 +0800

    Commit

---
+++ main.py
@@ -0,0 +1,9 @@
+echo "# pythonProject" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Bananaman56036/pythonProject.git
+git push -u origin main
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
